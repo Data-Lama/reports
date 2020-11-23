a2e94af95c64dfce0759b8c6ab3752f636bb73d6
Juan sanguino <jc.sanguino10@uniandes.edu.co>
2020-11-23 14:58:41 -0500

    Updates

---
+++ precache-manifest.6695b7fc8cbe35c76f46d6dc530509e9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0035cc91a26bb8440ba50552f43cdfe5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a757b4d7169397f9689",
+    "url": "/static/css/main.c24a821b.chunk.css"
+  },
+  {
+    "revision": "568d88a69b8414cb435a",
+    "url": "/static/js/2.9ff06b0a.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/static/js/2.9ff06b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a757b4d7169397f9689",
+    "url": "/static/js/main.5c63a759.chunk.js"
+  },
+  {
+    "revision": "6961748f970cfac7b692",
+    "url": "/static/js/runtime-main.336c0732.js"
+  },
+  {
+    "revision": "92c642e3ef835913ca6bc05e2e9d69b6",
+    "url": "/static/media/BG_triangles.92c642e3.png"
+  },
+  {
+    "revision": "56b092e2b4d3ad1973ae3abca067fe9a",
+    "url": "/static/media/Banner.56b092e2.jpg"
+  },
+  {
+    "revision": "8a527b9eb9a85e69dbc6cd2d3dc31c83",
+    "url": "/static/media/GO.8a527b9e.svg"
+  },
+  {
+    "revision": "78963aae5ece8c4e499d06d1c5d52fdc",
+    "url": "/static/media/LOGOBLANCO.78963aae.png"
+  },
+  {
+    "revision": "3e3041ec5f9d8afe7a0c4494e06fb8f1",
+    "url": "/static/media/Polygon 7.3e3041ec.svg"
+  },
+  {
+    "revision": "cea8c657122aaf07e9965620476529e7",
+    "url": "/static/media/Polygon 7on.cea8c657.svg"
+  },
+  {
+    "revision": "406c5128503018828dd04133088d6f51",
+    "url": "/static/media/footer.406c5128.svg"
+  },
+  {
+    "revision": "aa31cb783a1bdd513b0422a0915485ff",
+    "url": "/static/media/p1.aa31cb78.png"
+  },
+  {
+    "revision": "6eddc6f743e2df69abc46a4a80554542",
+    "url": "/static/media/p10.6eddc6f7.png"
+  },
+  {
+    "revision": "3c8384a7cdb1715da84ae1b5fc6b01f0",
+    "url": "/static/media/p2.3c8384a7.png"
+  },
+  {
+    "revision": "ce8f5edcc2093f3c8984ac3bee7330ab",
+    "url": "/static/media/p3.ce8f5edc.png"
+  },
+  {
+    "revision": "1e2680517e6776b270e11e79e9b0045e",
+    "url": "/static/media/p4.1e268051.png"
+  },
+  {
+    "revision": "9537fee1e26798a706def05f37b1af4e",
+    "url": "/static/media/p5.9537fee1.png"
+  },
+  {
+    "revision": "dd06673e10ee1b4815bc66ec1e29c15b",
+    "url": "/static/media/p6.dd06673e.png"
+  },
+  {
+    "revision": "aa689127f66ca2d3589dc1623e84c602",
+    "url": "/static/media/p7.aa689127.png"
+  },
+  {
+    "revision": "cf1a3693e56efee9660b117d1a4856d8",
+    "url": "/static/media/p8.cf1a3693.png"
+  },
+  {
+    "revision": "694c0090bfe2f014f9b425398d3339f2",
+    "url": "/static/media/p9.694c0090.png"
+  }
+]);
